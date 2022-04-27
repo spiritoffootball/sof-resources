c1ca0af93425f4dabc0741c7077bb85bc71249fc
Christian Wach <needle@haystack.co.uk>
2022-04-27 13:02:07 +0100

    Codestyle

--- includes/sof-resources-cpt.php
+++ includes/sof-resources-cpt.php
@@ -1,64 +1,66 @@
 <?php
+/**
+ * Custom Post Type Class.
+ *
+ * Handles the Custom Post Type for Quotes.
+ *
+ * @since 0.1
+ *
+ * @package Spirit_Of_Football_Quotes
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
 
 /**
- * SOF Resources Custom Post Type Class
+ * Custom Post Type Class.
  *
- * A class that encapsulates a Custom Post Types for Resources
+ * A class that encapsulates a Custom Post Types for Resources.
  *
- * @package WordPress
- * @subpackage SOF
+ * @since 0.1
  */
 class Spirit_Of_Football_Resources_CPT {
 
-
-
 	/**
-	 * Constructor
+	 * Constructor.
 	 *
 	 * @since 0.1
 	 */
 	public function __construct() {
 
-		// nothing
+		// Nothing.
 
 	}
 
-
-
 	/**
-	 * Register WordPress hooks
+	 * Register WordPress hooks.
 	 *
 	 * @since 0.1
 	 */
 	public function register_hooks() {
 
-		// always create post types
-		add_action( 'init', array( $this, 'create_post_type' ) );
+		// Always create post types.
+		add_action( 'init', [ $this, 'create_post_type' ] );
 
-		// make sure our feedback is appropriate
-		add_filter( 'post_updated_messages', array( $this, 'updated_messages' ) );
+		// Make sure our feedback is appropriate.
+		add_filter( 'post_updated_messages', [ $this, 'updated_messages' ] );
 
 	}
 
-
-
-
 	/**
-	 * Actions to perform on plugin activation
+	 * Actions to perform on plugin activation.
 	 *
 	 * @since 0.1
 	 */
 	public function activate() {
 
-		// pass through
+		// Pass through.
 		$this->create_post_type();
 
-		// go ahead and flush
+		// Go ahead and flush.
 		flush_rewrite_rules();
 
 	}
-
-
 
 	/**
 	 * Actions to perform on plugin deactivation (NOT deletion)
@@ -67,35 +69,32 @@ class Spirit_Of_Football_Resources_CPT {
 	 */
 	public function deactivate() {
 
-		// flush rules to reset
+		// Flush rules to reset.
 		flush_rewrite_rules();
 
 	}
 
-
-
-	// #########################################################################
-
-
+	// -------------------------------------------------------------------------
 
 	/**
-	 * Create our Custom Post Type
+	 * Create our Custom Post Type.
 	 *
 	 * @since 0.1
 	 */
 	public function create_post_type() {
 
-		// only call this once
+		// Only call this once.
 		static $registered;
 
-		// bail if already done
-		if ( $registered ) return;
+		// Bail if already done.
+		if ( $registered ) {
+			return;
+		}
 
-		// set up the post type called "Resource"
+		// Set up the post type called "Resource".
 		register_post_type( 'resource',
-
-			array(
-				'labels' => array(
+			[
+				'labels' => [
 					'name' => __( 'Resources', 'sof-resources' ),
 					'singular_name' => __( 'Resource', 'sof-resources' ),
 					'add_new' => _x( 'Add New', 'resource', 'sof-resources' ),
@@ -110,123 +109,118 @@ class Spirit_Of_Football_Resources_CPT {
 					'item_scheduled' => __( 'Resource scheduled.', 'sof-resources' ),
 					'item_updated' => __( 'Resource updated.', 'sof-resources' ),
 					'search_items' => __( 'Search Resources', 'sof-resources' ),
-					'not_found' =>  __( 'No matching Resource found', 'sof-resources' ),
+					'not_found' => __( 'No matching Resource found', 'sof-resources' ),
 					'not_found_in_trash' => __( 'No Resources found in Trash', 'sof-resources' ),
 					'parent_item_colon' => '',
 					'menu_name' => __( 'Resources', 'sof-resources' ),
-				),
+				],
 				'public' => true,
 				'publicly_queryable' => true,
 				'has_archive' => true,
 				'show_ui' => true,
-				'rewrite' => array(
+				'rewrite' => [
 					'slug' => 'resources',
-					'with_front' => false
-				),
+					'with_front' => false,
+				],
 				'query_var' => true,
 				'capability_type' => 'post',
 				'hierarchical' => false,
 				'show_in_nav_menus' => false,
 				'menu_position' => 5,
 				'exclude_from_search' => false,
-				'supports' => array(
+				'supports' => [
 					'title',
-					'editor'
-				),
-			)
-
+					'editor',
+				],
+			]
 		);
 
-		//flush_rewrite_rules();
-
-		// flag
+		// Flag.
 		$registered = true;
 
 	}
 
-
-
 	/**
-	 * Override messages for a custom post type
+	 * Override messages for a custom post type.
 	 *
-	 * @param array $messages The existing messages
-	 * @return array $messages The modified messages
+	 * @param array $messages The existing messages.
+	 * @return array $messages The modified messages.
 	 */
 	public function updated_messages( $messages ) {
 
-		// access relevant globals
+		// Access relevant globals.
 		global $post, $post_ID;
 
-		// define custom messages for our custom post type
-		$messages['resource'] = array(
+		// Define custom messages for our custom post type.
+		$messages['resource'] = [
 
-			// unused - messages start at index 1
+			// Unused - messages start at index 1.
 			0 => '',
 
-			// item updated
+			// Item updated.
 			1 => sprintf(
+				/* translators: %s: Post permalink URL. */
 				__( 'Resource updated. <a href="%s">View resource</a>', 'sof-resources' ),
 				esc_url( get_permalink( $post_ID ) )
 			),
 
-			// custom fields
+			// Custom fields.
 			2 => __( 'Custom field updated.', 'sof-resources' ),
 			3 => __( 'Custom field deleted.', 'sof-resources' ),
 			4 => __( 'Resource updated.', 'sof-resources' ),
 
-			// item restored to a revision
+			// Item restored to a revision.
 			5 => isset( $_GET['revision'] ) ?
 
-					// revision text
-					sprintf(
-						// translators: %s: date and time of the revision
-						__( 'Resource restored to revision from %s', 'sof-resources' ),
-						wp_post_revision_title( (int) $_GET['revision'], false )
-					) :
+				// Revision text.
+				sprintf(
+					/* translators: %s: Title of the revision. */
+					__( 'Resource restored to revision from %s', 'sof-resources' ),
+					wp_post_revision_title( (int) $_GET['revision'], false )
+				) :
 
-					// no revision
-					false,
+				// No revision.
+				false,
 
-			// item published
+			// Item published.
 			6 => sprintf(
+				/* translators: %s: Post permalink URL. */
 				__( 'Resource published. <a href="%s">View resource</a>', 'sof-resources' ),
 				esc_url( get_permalink( $post_ID ) )
 			),
 
-			// item saved
+			// Item saved.
 			7 => __( 'Resource saved.', 'sof-resources' ),
 
-			// item submitted
+			// Item submitted.
 			8 => sprintf(
+				/* translators: %s: Post preview URL. */
 				__( 'Resource submitted. <a target="_blank" href="%s">Preview resource</a>', 'sof-resources' ),
 				esc_url( add_query_arg( 'preview', 'true', get_permalink( $post_ID ) ) )
 			),
 
-			// item scheduled
+			// Item scheduled.
 			9 => sprintf(
+				/* translators: 1: Publish box date format, see http://php.net/date, 2: Post date, 3: Post permalink. */
 				__( 'Resource scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview resource</a>', 'sof-resources' ),
-				// translators: Publish box date format, see http://php.net/date
-				date_i18n( __( 'M j, Y @ G:i' ),
+				/* translators: Publish box date format, see http://php.net/date */
+				date_i18n( __( 'M j, Y @ G:i', 'sof-resources' ),
 				strtotime( $post->post_date ) ),
 				esc_url( get_permalink( $post_ID ) )
 			),
 
-			// draft updated
+			// Draft updated.
 			10 => sprintf(
+				/* translators: %s: Post preview URL. */
 				__( 'Resource draft updated. <a target="_blank" href="%s">Preview resource</a>', 'sof-resources' ),
 				esc_url( add_query_arg( 'preview', 'true', get_permalink( $post_ID ) ) )
-			)
+			),
 
-		);
+		];
 
 		// --<
 		return $messages;
 
 	}
 
-
-
-} // class Spirit_Of_Football_Resources_CPT ends
-
-
-
+}
--- includes/sof-resources-metaboxes.php
+++ includes/sof-resources-metaboxes.php
@@ -1,172 +1,172 @@
 <?php
+/**
+ * Metaboxes Class.
+ *
+ * Handles Metaboxes for Resources.
+ *
+ * @since 0.1
+ *
+ * @package Spirit_Of_Football_Resources
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
 
 /**
- * SOF Resources Metaboxes Class
+ * Metaboxes Class.
  *
- * A class that encapsulates all Metaboxes for Resources
+ * A class that encapsulates all Metaboxes for Resources.
  *
- * @package WordPress
- * @subpackage SOF
+ * @since 0.1
  */
 class Spirit_Of_Football_Resources_Metaboxes {
-
-
 
 	/**
 	 * Sticky Resource meta key
 	 *
 	 * @since 0.1
 	 * @access public
-	 * @var str $meta_key The meta key for sticky resources
+	 * @var str $meta_key The meta key for the sticky Resource.
 	 */
 	public $sticky_meta_key = 'resource_sticky';
 
-
-
 	/**
-	 * Constructor
+	 * Constructor.
 	 *
 	 * @since 0.1
 	 */
 	public function __construct() {
 
-		// nothing
+		// Nothing.
 
 	}
 
-
-
 	/**
-	 * Register WordPress hooks
+	 * Register WordPress hooks.
 	 *
 	 * @since 0.1
 	 */
 	public function register_hooks() {
 
-		// exclude from SOF eV for now...
-		//if ( 'sofev' == sof_get_site() ) return;
+		// Add meta boxes.
+		add_action( 'add_meta_boxes', [ $this, 'add_meta_boxes' ] );
 
-		// add meta boxes
-		add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
-
-		// intercept save
-		add_action( 'save_post', array( $this, 'save_post' ), 1, 2 );
+		// Intercept save.
+		add_action( 'save_post', [ $this, 'save_post' ], 1, 2 );
 
 	}
 
-
-
-
-	// #########################################################################
-
-
+	// -------------------------------------------------------------------------
 
 	/**
-	 * Adds meta boxes to admin screens
+	 * Adds meta boxes to admin screens.
 	 *
 	 * @since 0.1
 	 */
 	public function add_meta_boxes() {
 
-		// add our meta box
+		// Add our meta box.
 		add_meta_box(
 			'sof_resource_options',
 			__( 'Stick at top', 'sof-resources' ),
-			array( $this, 'resource_box' ),
+			[ $this, 'resource_box' ],
 			'resource',
 			'side'
 		);
 
 	}
 
-
-
 	/**
-	 * Adds a "Stick at top" meta box to Resource edit screens
+	 * Adds a "Stick at top" meta box to Resource edit screens.
 	 *
 	 * @since 0.1
-	 * @param WP_Post $post The object for the current post/page
+	 *
+	 * @param WP_Post $post The object for the current post/page.
 	 */
 	public function resource_box( $post ) {
 
-		// Use nonce for verification
+		// Use nonce for verification.
 		wp_nonce_field( 'sof_resource_settings', 'sof_resource_nonce' );
 
-		// set key
+		// Set key.
 		$db_key = '_' . $this->sticky_meta_key;
 
-		// default to empty
+		// Default to empty.
 		$val = '';
 
-		// get value if if the custom field already has one
+		// Get value if if the custom field already has one.
 		$existing = get_post_meta( $post->ID, $db_key, true );
 		if ( ! empty( $existing ) ) {
 			$val = get_post_meta( $post->ID, $db_key, true );
 		}
 
-		// open
+		// Open.
 		echo '<p>';
 
-		// checkbox
-		echo '<input id="' . $this->sticky_meta_key . '" name="' . $this->sticky_meta_key . '" value="1" type="checkbox" ' . (($val == '1') ? ' checked="checked"' : '') . '/>';
+		// Checkbox.
+		echo '<input id="' . $this->sticky_meta_key . '" name="' . $this->sticky_meta_key . '" value="1" type="checkbox" ' . ( ( $val == '1' ) ? ' checked="checked"' : '' ) . '/>';
 
-		// construct label
+		// Construct label.
 		echo '<strong><label for="' . $this->sticky_meta_key . '">' . __( 'Stick at top of lists', 'sof-resources' ) . '</label></strong>';
 
-		// close
+		// Close.
 		echo '</p>';
 
 	}
 
-
-
 	/**
-	 * Stores our additional params
+	 * Stores our additional params.
 	 *
 	 * @since 0.1
-	 * @param integer $post_id the ID of the post (or revision)
-	 * @param integer $post the post object
+	 *
+	 * @param integer $post_id The ID of the post or revision.
+	 * @param integer $post The post object.
 	 */
 	public function save_post( $post_id, $post ) {
 
-		// we don't use post_id because we're not interested in revisions
+		// We don't use post_id because we're not interested in revisions.
 
-		// store our page meta data
-		$result = $this->_save_page_meta( $post );
+		// Store our page meta data.
+		$result = $this->save_page_meta( $post );
 
 	}
 
-
-
-	// #########################################################################
-
-
+	// -------------------------------------------------------------------------
 
 	/**
-	 * When a page is saved, this also saves the options
+	 * When a page is saved, this also saves the options.
 	 *
 	 * @since 0.1
-	 * @param WP_Post $post_obj The object for the post (or revision)
+	 *
+	 * @param WP_Post $post_obj The object for the post or revision.
 	 */
-	private function _save_page_meta( $post_obj ) {
+	private function save_page_meta( $post_obj ) {
 
-		// if no post, kick out
-		if ( ! $post_obj ) return;
+		// Bail if no post.
+		if ( ! $post_obj ) {
+			return;
+		}
 
-		// authenticate
+		// Authenticate.
 		$nonce = isset( $_POST['sof_resource_nonce'] ) ? $_POST['sof_resource_nonce'] : '';
-		if ( ! wp_verify_nonce( $nonce, 'sof_resource_settings' ) ) return;
+		if ( ! wp_verify_nonce( $nonce, 'sof_resource_settings' ) ) {
+			return;
+		}
 
-		// is this an auto save routine?
-		if ( defined('DOING_AUTOSAVE') AND DOING_AUTOSAVE ) return;
+		// Is this an auto save routine?
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			return;
+		}
 
-		// Check permissions
-		if ( ! current_user_can( 'edit_page', $post_obj->ID ) ) return;
+		// Check permissions.
+		if ( ! current_user_can( 'edit_page', $post_obj->ID ) ) {
+			return;
+		}
 
-		// check for revision
+		// Check for revision.
 		if ( $post_obj->post_type == 'revision' ) {
 
-			// get parent
+			// Get parent.
 			if ( $post_obj->post_parent != 0 ) {
 				$post = get_post( $post_obj->post_parent );
 			} else {
@@ -177,38 +177,39 @@ class Spirit_Of_Football_Resources_Metaboxes {
 			$post = $post_obj;
 		}
 
-		// bail if not resource post type
-		if ( $post->post_type == 'resource' ) return;
+		// Bail if not resource post type.
+		if ( $post->post_type == 'resource' ) {
+			return;
+		}
 
 		// ---------------------------------------------------------------------
-		// okay, we're through...
+		// Okay, we're through...
 		// ---------------------------------------------------------------------
 
-		// define key
+		// Define prefixed key.
 		$db_key = '_' . $this->sticky_meta_key;
 
-		// get sticky value
-		$is_sticky = ( isset( $_POST[$this->sticky_meta_key] ) ) ? '1' : '0';
+		// Get sticky value.
+		$is_sticky = ( isset( $_POST[ $this->sticky_meta_key ] ) ) ? '1' : '0';
 
-		// save for this post
-		$this->_save_meta( $post, $db_key, $is_sticky );
+		// Save for this post.
+		$this->save_meta( $post, $db_key, $is_sticky );
 
-		// delete this meta value from all other resources because only one
-		// resource can be sticky
+		// Delete this meta value from all other resources because only one Resource can be sticky.
 		if ( $is_sticky == '1' ) {
 
-			// get all resources
-			$resources = get_posts( array(
-				'post_type' => 'resource'
-			) );
+			// Get all Resources.
+			$resources = get_posts( [
+				'post_type' => 'resource',
+			] );
 
-			// if we have any
+			// If we have any.
 			if ( count( $resources ) > 0 ) {
 
-				// loop and save, excluding current post
-				foreach( $resources AS $resource ) {
+				// Loop and save, excluding current post.
+				foreach ( $resources as $resource ) {
 					if ( $post->ID != $resource->ID ) {
-						$this->_save_meta( $post, $db_key, '0' );
+						$this->save_meta( $post, $db_key, '0' );
 					}
 				}
 
@@ -218,29 +219,28 @@ class Spirit_Of_Football_Resources_Metaboxes {
 
 	}
 
-
-
 	/**
-	 * Utility to automate meta data saving
+	 * Utility to automate meta data saving.
 	 *
 	 * @since 0.1
-	 * @param WP_Post $post_obj The WordPress post object
-	 * @param string $key The meta key
-	 * @param mixed $data The data to be saved
-	 * @return mixed $data The data that was saved
+	 *
+	 * @param WP_Post $post The WordPress post object.
+	 * @param string $key The meta key.
+	 * @param mixed $data The data to be saved.
+	 * @return mixed $data The data that was saved.
 	 */
-	private function _save_meta( $post, $key, $data = '' ) {
+	private function save_meta( $post, $key, $data = '' ) {
 
-		// if the custom field already has a value...
+		// If the custom field already has a value.
 		$existing = get_post_meta( $post->ID, $key, true );
 		if ( ! empty( $existing ) ) {
 
-			// update the data
+			// Update the data.
 			update_post_meta( $post->ID, $key, $data );
 
 		} else {
 
-			// add the data
+			// Add the data.
 			add_post_meta( $post->ID, $key, $data );
 
 		}
@@ -250,10 +250,4 @@ class Spirit_Of_Football_Resources_Metaboxes {
 
 	}
 
-
-
-
-} // class Spirit_Of_Football_Resources_Metaboxes ends
-
-
-
+}
